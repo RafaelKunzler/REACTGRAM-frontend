c900ecca254c4926012cd1d6ff64456dbe40c6b4
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-12-09 11:09:03 -0300

    router config

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+// router
+import {Routes, Route, Navigate, BrowserRouter} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
