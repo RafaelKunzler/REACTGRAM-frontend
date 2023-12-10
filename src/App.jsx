55d5e6e64e7c46787f5a9dc4fceeab0cc457c069
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-12-10 11:23:04 -0300

    auth navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 // router
 import {Routes, Route, Navigate, BrowserRouter} from 'react-router-dom'
 
+// hooks
+import { useAuth } from './hooks/useAuth'
+
 // pages
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
@@ -13,6 +16,12 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
+
+  const {auth, loading} = useAuth()
+
+  if(loading){
+    return <p>Carregando...</p>
+  }
   
   return (
     <div className="App">
@@ -20,9 +29,9 @@ function App() {
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
+          <Route path='/' element={auth ? <Home /> : <Navigate to='/login'/>}/>
+          <Route path='/login' element={!auth ? <Login /> : <Navigate to ='/'/>}/>
+          <Route path='/register' element={!auth ? <Register /> : <Navigate to ='/'/>}/>
         </Routes>
       </div>
       <Footer />
